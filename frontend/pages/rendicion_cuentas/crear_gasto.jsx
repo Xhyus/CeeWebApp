b46d36a3e524ac1f3b509c309216a17d11e274d2
PabloIMG <pablo16mg@gmail.com>
2022-05-23 00:18:13 -0400

    Recopilacion de datos del formulario

--- frontend/pages/rendicion_cuentas/crear_gasto.jsx
+++ frontend/pages/rendicion_cuentas/crear_gasto.jsx
@@ -4,12 +4,43 @@ import styles from "./rendicion_cuentas.module.css";
 export default function crear_gasto() {
 
     //* .: DATOS DEL GASTO :. *//
-    const [asunto, setAsunto] = useState('');
-    const [totalGastado, setTotalGastado] = useState(0);
-    const [tipoGasto, setTipoGasto] = useState('');
-    const [fechaGasto, setFechaGasto] = useState('');
-    const [detalle, setDetalle] = useState('');
-    const [boleta, setBoleta] = useState('');
+    const [datosGasto, setDatosGasto] = useState({
+        asunto: '',
+        total: '',
+        tipo: '',
+        fecha: '',
+        detalle: '',
+    })
+
+    //! Instrucción temporal
+    const [boletaGasto, setBoletaGasto] = useState("/carpeta/foto/boleta")
+
+    //? Función para capturar cambios en los inputs.
+    const handleInputChange = (event) => {
+
+        setDatosGasto({
+
+            ...datosGasto,
+            [event.target.name] : event.target.value
+
+        })
+
+    }
+
+    //? Función prueba para evaluar datos ingresados.
+    const enviarDatos = () => {
+
+        //? Bandera de información guardada.
+        console.log("Asunto        : " + datosGasto.asunto);
+        console.log("Total Gastado : " + datosGasto.total);
+        console.log("Tipo Gasto    : " + datosGasto.tipo);
+        console.log("Fecha Gasto   : " + datosGasto.fecha);
+        console.log("Detalle       : " + datosGasto.detalle);
+        console.log("Boleta        : " + boletaGasto + "\n");
+
+        //* Enviar datos a la API.
+        
+    }
 
     return (
 
@@ -32,13 +63,13 @@ export default function crear_gasto() {
                         {/* Ingresar asunto del gasto */}
                         <div>
                             <p className = {styles.Propiedades_texto}>Asunto</p>
-                            <input type="text" />
+                            <input type="text" name="asunto" onChange={handleInputChange} />
                         </div>
 
                         {/* Ingresar total del gasto */}
                         <div>
                             <p className = {styles.Propiedades_texto}>Total</p>
-                            <input type="number" />
+                            <input type="number" name="total" onChange={handleInputChange} />
                         </div>
                         
                     </div>
@@ -49,13 +80,13 @@ export default function crear_gasto() {
                         {/* Ingresar tipo de gasto (Ingreso o gasto) */}
                         <div>
                             <p className = {styles.Propiedades_texto}>Tipo</p>
-                            <input type="text" />
+                            <input type="text" name="tipo" onChange={handleInputChange} />
                         </div>
 
                         {/* Ingresar fecha en que se realizó el gasto */}
                         <div>
                             <p className = {styles.Propiedades_texto}>Fecha</p>
-                            <input type="text" />
+                            <input type="text" name="fecha" onChange={handleInputChange} />
                         </div>
 
                     </div>
@@ -63,18 +94,18 @@ export default function crear_gasto() {
                     {/* .: DETALLE :. */}
                     <div className = {styles.Contenedor_detalle}>
                         <h3 className = {styles.Propiedades_texto}>Detalle</h3>
-                        <input type="text" />
+                        <input type="text" name="detalle" onChange={handleInputChange} />
                     </div>
 
                     {/* .: BOLETA :. */}
                     <div className = {styles.Contenedor_boleta}>
                         <h3 className = {styles.Propiedades_texto}>Boleta</h3>
-                        <input type="file" />
+                        <input type="file" name="boleta" onChange={handleInputChange} />
                     </div>
 
                     {/* .: ENVIAR :. */}
                     <div className = {styles.Contenedor_boton}>
-                        <button className = {styles.Propiedades_texto}>Enviar</button>
+                        <button className = {styles.Propiedades_texto} onClick = {() => enviarDatos()}>Enviar</button>
                     </div>
 
                 </div>
