2eac83ae6dfa0021956f52fc303e23259fbd1ec2
Ignacio González <ignacio.gonzalez1901@alumnos.ubiobio.cl>
2022-08-03 00:09:33 -0400

    BUILD FOR FRONTEND

--- backend/middlewares/fileSize.js
+++
@@ -1,9 +0,0 @@
-const fileSizeError = (err, req, res, next) => {
-    if (err) {
-        res.status(413).send({ message: 'El archivo es demasiado grande' })
-    } else {
-        next()
-    }
-}
-
-module.exports = fileSizeError;
