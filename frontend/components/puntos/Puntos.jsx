7a6a425454849de85f027046091fe568c977ce72
Ignacio González <ignacio.gonzalez1901@alumnos.ubiobio.cl>
2022-08-19 01:13:52 -0400

    Se agregan funcionalidad de eliminar puntos junto a su icono

--- frontend/components/puntos/Puntos.jsx
+++ frontend/components/puntos/Puntos.jsx
@@ -1,11 +1,27 @@
 import React from 'react'
 import styles from '../../styles/crear_asambleas.module.css'
+import { FaTrash } from 'react-icons/fa'
 
-const Puntos = ({ handleChangePunto, id }) => {
+const Puntos = ({ handleChangePunto, id, handleDeletePunto }) => {
+
+    const handleDelete = () => {
+        if (id !== 0) {
+            return (
+                <div className={styles.contenedorBoton}>
+                    <FaTrash className={styles.icono} onClick={() => handleDeletePunto(id)} />
+                </div>
+            )
+        }
+    }
+
     return (
         <div className={styles.contenedorPuntos}>
-            <label htmlFor={id}>Punto a tratar: {id + 1}</label>
-            <input type="text" name={id} placeholder="Punto a tratar" onChange={handleChangePunto} className={styles.inputs} />
+            <div className={styles.contenedorPunto}>
+
+                <label htmlFor={id}>Punto a tratar: {id + 1}</label>
+                <input type="text" name={id} placeholder="Punto a tratar" onChange={handleChangePunto} className={styles.inputs} />
+            </div>
+            {handleDelete()}
         </div>
     )
 }
--- frontend/pages/asambleas/crear.jsx
+++ frontend/pages/asambleas/crear.jsx
@@ -3,23 +3,23 @@ import Navbar from '../../components/navbar/Navbar'
 import { isLogged } from '../../utils/logged'
 import styles from '../../styles/crear_asambleas.module.css'
 import Puntos from '../../components/puntos/Puntos'
-import { FaPlus } from 'react-icons/fa'
+import { FaPlus, FaTrash } from 'react-icons/fa'
+import axios from 'axios'
+import Swal from 'sweetalert2'
 
 const crear = () => {
     const [asamblea, setAsamblea] = useState({
         asunto: '',
         fecha: '',
         tipoAsamblea: '',
-        puntos: []
+        contexto: '',
     })
     const [puntos, setPuntos] = useState([{
         id: 0,
         asunto: ''
     }])
 
-    useEffect(() => {
-        isLogged()
-    }, [])
+    useEffect(() => isLogged(), [])
 
     const handleChange = (e) => {
         setAsamblea({
@@ -28,15 +28,20 @@ const crear = () => {
         })
     }
 
-    const handleSubmit = () => {
+    const handleAddPunto = () => {
+        const ultimoId = puntos[puntos.length - 1].id
+        setPuntos([...puntos, {
+            id: ultimoId + 1,
+            asunto: ''
+        }])
+    }
+    const handleDeletePunto = (id) => {
+        setPuntos(puntos.filter(punto => punto.id !== id))
         puntos.map(punto => {
-            asamblea.puntos.push(punto.asunto)
+            if (punto.id > id) {
+                punto.id = punto.id - 1
+            }
         })
-        setAsamblea({
-            ...asamblea,
-            puntos: puntos
-        })
-        console.log("asunto: ", asamblea.asunto, "fecha: ", asamblea.fecha, "tipoAsamblea: ", asamblea.tipoAsamblea, "puntos: ", asamblea.puntos)
     }
 
     const handleChangePunto = (e) => {
@@ -53,12 +58,60 @@ const crear = () => {
         )
     }
 
-    const handleAddPunto = () => {
-        const ultimoId = puntos[puntos.length - 1].id
-        setPuntos([...puntos, {
-            id: ultimoId + 1,
-            asunto: ''
-        }])
+    const handleSubmit = () => {
+        if (asamblea.asunto === '' || asamblea.fecha === '' || asamblea.tipoAsamblea === '' || asamblea.contexto === '') {
+            Swal.fire({
+                title: 'Error',
+                text: 'Todos los campos son obligatorios',
+                icon: 'error',
+                confirmButtonText: 'Aceptar'
+            })
+        } else {
+            let carrera = localStorage.getItem('carrera')
+            axios.post(process.env.SERVIDOR + '/asamblea/' + carrera, {
+                asunto: asamblea.asunto,
+                fecha: asamblea.fecha,
+                tipoAsamblea: asamblea.tipoAsamblea,
+                contexto: asamblea.contexto,
+            })
+                .then(res => {
+                    postPunto(res.data._id)
+                })
+                .catch(err => {
+                    Swal.fire({
+                        title: 'Error',
+                        text: 'Error al crear la asamblea',
+                        icon: 'error',
+                        confirmButtonText: 'Aceptar'
+                    })
+                })
+        }
+
+    }
+
+    const postPunto = (id) => {
+        puntos.map(punto => {
+            let data = {
+                asunto: punto.asunto,
+                descripcion: ""
+            }
+            axios.post(process.env.SERVIDOR + '/punto/' + id, data)
+                .then(res => {
+                    Swal.fire({
+                        title: 'Exito',
+                        text: 'Asamblea creada con exito',
+                        icon: 'success',
+                        confirmButtonText: 'Aceptar'
+                    })
+                }).catch(err => {
+                    Swal.fire({
+                        title: 'Error',
+                        text: 'Error al crear los puntos',
+                        icon: 'error',
+                        confirmButtonText: 'Aceptar'
+                    })
+                })
+        })
     }
 
     return (
@@ -67,15 +120,19 @@ const crear = () => {
             <div className={styles.fondo}>
                 <div className={styles.contenedor}>
                     <div className={styles.contenedorCrear}>
-                        <p className={styles.titulo_filtro}><strong>Crear Asamblea</strong></p>
+                        <h1 className={styles.titulo_filtro}>Crear Asamblea</h1>
                         <section className={styles.contenedores}>
                             <div className={styles.contenedorAsunto}>
                                 <label className={styles.labels}>Asunto: </label>
-                                <input className={styles.inputs} name="asunto" id='asunto' type="text" placeholder="Asunto" onChange={handleChange} />
+                                <input className={styles.inputs} name="asunto" required id='asunto' type="text" placeholder="Asunto asamblea" onChange={handleChange} />
+                            </div>
+                            <div className={styles.contenedorContexto}>
+                                <label className={styles.labels}>Contexto Asamblea: </label>
+                                <textarea className={styles.textarea} name="contexto" required id='contexto' type="text" placeholder="Contexto asamblea" onChange={handleChange} />
                             </div>
                             <div className={styles.contenedorTipoAsamblea}>
                                 <label className={styles.labels}>Tipo de asamblea: </label>
-                                <select className={styles.selectTipo} name="selectTipo" id='selectTipo' onChange={handleChange}>
+                                <select className={styles.selectTipo} required name="tipoAsamblea" id='tipoAsamblea' onChange={handleChange}>
                                     <option value="">Seleccione un tipo de asamblea</option>
                                     <option value="resolutiva">Resolutiva</option>
                                     <option value="informativa">Informativa</option>
@@ -83,15 +140,15 @@ const crear = () => {
                             </div>
                             <div className={styles.contenedorFecha}>
                                 <label className={styles.labels}>Fecha: </label>
-                                <input className={styles.inputs} type="datetime-local" onChange={handleChange} name="fecha" id="input-fecha" />
+                                <input className={styles.inputs} required type="datetime-local" onChange={handleChange} name="fecha" id="input-fecha" />
                             </div>
-                            <FaPlus onClick={() => handleAddPunto()} />
                             {puntos.map((punto, index) => {
                                 return (
-                                    <Puntos key={index} handleChangePunto={handleChangePunto} id={punto.id} />
+                                    <Puntos key={index} handleChangePunto={handleChangePunto} id={punto.id} handleDeletePunto={handleDeletePunto} />
                                 )
                             })
                             }
+                            <FaPlus className={styles.icon} onClick={() => handleAddPunto()} />
                             <button className={`${styles.Propiedades_boton} ${styles.crear}`} onClick={() => handleSubmit()}>Crear Asamblea</button>
                         </section>
                     </div>
