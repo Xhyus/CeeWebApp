8fe520ac83cca29dc41d64ac3ea560f3a41eea00
Ignacio González <ignacio.gonzalez1901@alumnos.ubiobio.cl>
2022-08-18 20:41:58 -0400

    Generacion archivo css de crear asamblea, Reiteracion de estilos css y reiteracion manejo de estados

--- frontend/pages/asambleas/crear.jsx
+++ frontend/pages/asambleas/crear.jsx
@@ -1,33 +1,58 @@
 import React, { useState, useEffect } from 'react'
 import Navbar from '../../components/navbar/Navbar'
 import { isLogged } from '../../utils/logged'
-import styles from '../../styles/asambleas.module.css'
+import styles from '../../styles/crear_asambleas.module.css'
 
 const crear = () => {
-    const [asunto, setAsunto] = useState()
-    const [fecha, setFecha] = useState()
-    const [tipoAsamblea, setTipoAsamblea] = useState(null)
-    const [boton, setboton] = useState('Crear')
+    const [asamblea, setAsamblea] = useState({
+        asunto: '',
+        fecha: '',
+        tipoAsamblea: '',
+        puntos: []
+    })
+    const [puntos, setPuntos] = useState([{
+        asunto: '',
+        id: ''
+    }])
+    const [cantidadPuntos, setCantidadPuntos] = useState(1)
 
     useEffect(() => {
         isLogged()
     }, [])
 
-    const handleChangeAsunto = (e) => {
-        setAsunto(e.target.value)
-    }
-
-    const handleChangeFecha = (e) => {
-        setFecha(e.target.value)
-    }
-
-    const handleChangeTipoAsamblea = (e) => {
-        setTipoAsamblea(e.target.value)
-        console.log(tipoAsamblea)
+    const handleChange = (e) => {
+        setAsamblea({
+            ...asamblea,
+            [e.target.name]: e.target.value
+        })
     }
 
     const handleSubmit = () => {
-        console.log(asunto, fecha, tipoAsamblea)
+        setAsamblea({
+            ...asamblea,
+            puntos: puntos
+        })
+        console.log("asunto: ", asamblea.asunto, "fecha: ", asamblea.fecha, "tipoAsamblea: ", asamblea.tipoAsamblea, "puntos: ", puntos)
+    }
+
+    const handleChangePunto = (e) => {
+        setPuntos({
+            id: e.target.id,
+            asunto: e.target.value
+        })
+        console.log("punto: ", puntos.asunto, "id: ", puntos.id)
+    }
+
+    const puntosList = () => {
+        let puntos = []
+        for (let i = 0; i < cantidadPuntos; i++) {
+            puntos.push(
+                <div className={styles.contenedorPuntos}>
+                    <input type="text" id={i} placeholder="Asunto a tratar" onChange={handleChangePunto} className={styles.inputs} />
+                </div>
+            )
+        }
+        return puntos
     }
 
     return (
@@ -36,25 +61,27 @@ const crear = () => {
             <div className={styles.fondo}>
                 <div className={styles.contenedor}>
                     <div className={styles.contenedorCrear}>
-                        <p className={styles.titulo_filtro}><strong>Filtro</strong></p>
+                        <p className={styles.titulo_filtro}><strong>Crear Asamblea</strong></p>
                         <section className={styles.contenedores}>
                             <div className={styles.contenedorAsunto}>
                                 <label className={styles.labels}>Asunto: </label>
-                                <input className={styles.inputs} name="Asunto" id='Asunto' type="text" placeholder="Asunto" onChange={handleChangeAsunto} />
+                                <input className={styles.inputs} name="asunto" id='asunto' type="text" placeholder="Asunto" onChange={handleChange} />
                             </div>
                             <div className={styles.contenedorTipoAsamblea}>
                                 <label className={styles.labels}>Tipo de asamblea: </label>
-                                <select className={styles.selectTipo} name="selectTipo" id='selectTipo' onChange={handleChangeTipoAsamblea}>
-                                    <option value="null">Seleccione un tipo de asamblea</option>
+                                <select className={styles.selectTipo} name="selectTipo" id='selectTipo' onChange={handleChange}>
+                                    <option value="">Seleccione un tipo de asamblea</option>
                                     <option value="resolutiva">Resolutiva</option>
                                     <option value="informativa">Informativa</option>
                                 </select>
                             </div>
                             <div className={styles.contenedorFecha}>
                                 <label className={styles.labels}>Fecha: </label>
-                                <input className={styles.inputs} type="datetime-local" onChange={handleChangeFecha} name="input-fecha" id="input-fecha" />
+                                <input className={styles.inputs} type="datetime-local" onChange={handleChange} name="fecha" id="input-fecha" />
                             </div>
-                            <button className={styles.Propiedades_boton} onClick={() => handleSubmit()}>{boton}</button>
+                            <button onClick={() => setCantidadPuntos(cantidadPuntos + 1)} > Puntos </button >
+                            {puntosList()}
+                            <button className={`${styles.Propiedades_boton} ${styles.crear}`} onClick={() => handleSubmit()}>Crear Asamblea</button>
                         </section>
                     </div>
                 </div>
